af671cf8243a664d9053b3af4631a6943a4a6db4
Thare <chenhao@zenzet.com>
2017-08-29 23:43:08 +0800

    update nowcoder.Test2017.d2cced737eb54a3aa550f53bb3cc19d0

--- src/nowcoder/Test2017/d2cced737eb54a3aa550f53bb3cc19d0/Main.java
+++ src/nowcoder/Test2017/d2cced737eb54a3aa550f53bb3cc19d0/Main.java
@@ -1,9 +1,6 @@
 package nowcoder.Test2017.d2cced737eb54a3aa550f53bb3cc19d0;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
