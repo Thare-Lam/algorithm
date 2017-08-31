036f41fa9a45e09c4ba24f1f21284eaed5ff2e6b
Thare <tharelam216@gmail.com>
2017-08-31 23:53:32 +0800

    ac leetcode._523_continuous_subarray_sum

---
+++ src/leetcode/_523_continuous_subarray_sum/Solution.java
@@ -0,0 +1,27 @@
+package leetcode._523_continuous_subarray_sum;
+
+import java.util.*;
+
+public class Solution {
+
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            if (k != 0) {
+                sum %= k;
+            }
+            if (map.containsKey(sum)) {
+                if (i - map.get(sum) > 1) {
+                    return true;
+                }
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return false;
+    }
+
+}
