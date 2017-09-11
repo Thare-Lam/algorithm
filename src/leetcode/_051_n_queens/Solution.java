50a27b0b11453e1b1e9306ab3abefa8e99a1eeb3
Thare <tharelam216@gmail.com>
2017-09-11 21:42:12 +0800

    ac leetcode._051_n_queens

--- src/leetcode/_051_n_queens/Solution.java
+++ src/leetcode/_051_n_queens/Solution.java
@@ -4,79 +4,61 @@ import java.util.*;
 
 public class Solution {
 
+    private List<List<String>> res = new ArrayList<>();
+
+    private List<Integer> list;
+
+    private int n;
+
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<>();
-        char[][] matrix = new char[n][n];
-        byte[][] visit = new byte[n][n];
-        for (int i = 0; i < n; ++i) {
-            Arrays.fill(matrix[i], '.');
-        }
-        process(res, matrix, visit, n, 0);
+        this.n = n;
+        list = new ArrayList<>(n);
+        process(0);
         return res;
     }
 
-    private void process(List<List<String>> res, char[][] matrix, byte[][] visit, int n, int k) {
+    private void process(int k) {
         for (int i = 0; i < n; ++i) {
-            if (visit[k][i] == 0) {
-                char[][] newMatrix = copy(matrix, n);
-                newMatrix[k][i] = 'Q';
-                if (k == n - 1) {
-                    res.add(convertCharArr(newMatrix, n));
-                } else {
-                    byte[][] newVisit = copy(visit, n);
-                    updateVisit(newVisit, n, k, i);
-                    process(res, newMatrix, newVisit, n, k + 1);
-                }
+            if (!check(k, i)) {
+                continue;
+            }
+            list.add(i);
+            if (k == n - 1) {
+                addRes();
+                list.remove(k);
+                break;
+            } else {
+                process(k + 1);
+                list.remove(k);
             }
         }
     }
 
-    private void updateVisit(byte[][] visit, int n, int x, int y) {
-        int i;
-        for (i = 0; i < n; ++i) {
-            visit[x][i] = 1;
-            visit[i][y] = 1;
-        }
-        int[] xd = new int[]{-1, -1, 1, 1};
-        int[] yd = new int[]{-1, 1, -1, 1};
-        int x1, y1;
-        for (i = 0; i < 4; ++i) {
-            x1 = x;
-            y1 = y;
-            while (x1 >= 0 && x1 < n && y1 >= 0 && y1 < n) {
-                visit[x1][y1] = 1;
-                x1 += xd[i];
-                y1 += yd[i];
+    private boolean check(int x, int y) {
+        int gapY;
+        for (int i = 0, size = list.size(); i < size; ++i) {
+            gapY = Math.abs(y - list.get(i));
+            if (gapY == 0 || gapY == Math.abs(x - i)) {
+                return false;
             }
         }
+        return true;
     }
 
-    private List<String> convertCharArr(char[][] m, int n) {
-        List<String> list = new ArrayList<>(n);
-        for (char[] s: m) {
-            list.add(String.valueOf(s));
-        }
-        return list;
-    }
-
-    private char[][] copy(char[][] m, int n) {
-        char[][] copy = new char[n][n];
+    private void addRes() {
+        char[] c;
+        List<String> sl = new ArrayList<>(n);
         for (int i = 0; i < n; ++i) {
-            copy[i] = Arrays.copyOf(m[i], n);
+            c = new char[n];
+            Arrays.fill(c, '.');
+            c[list.get(i)] = 'Q';
+            sl.add(String.valueOf(c));
         }
-        return copy;
-    }
-
-    private byte[][] copy(byte[][] b, int n) {
-        byte[][] copy = new byte[n][n];
-        for (int i = 0; i < n; ++i) {
-            copy[i] = Arrays.copyOf(b[i], n);
-        }
-        return copy;
+        res.add(sl);
     }
 
     public static void main(String[] args) {
-        for (List<String> list : new Solution().solveNQueens(6)) {
+        for (List<String> list: new Solution().solveNQueens(4)) {
             for (String s : list) {
                 System.out.println(s);
             }
