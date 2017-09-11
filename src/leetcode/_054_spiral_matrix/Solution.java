4a9834d9542f5b1d8854b648a2a8e1ba67abda04
Thare <tharelam216@gmail.com>
2017-09-11 22:20:17 +0800

    ac leetcode._054_spiral_matrix

---
+++ src/leetcode/_054_spiral_matrix/Solution.java
@@ -0,0 +1,63 @@
+package leetcode._054_spiral_matrix;
+
+import java.util.*;
+
+public class Solution {
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return new ArrayList<>();
+        }
+        int n = matrix[0].length;
+        List<Integer> res = new ArrayList<>(m * n);
+        int x = 0, y = 0;
+        int xMin = 0, xMax = m - 1, yMin = 0, yMax = n - 1;
+        while (xMin <= xMax && yMin <= yMax) {
+            while (y <= yMax) {
+                res.add(matrix[x][y++]);
+            }
+            --y;
+            ++x;
+            ++xMin;
+            if (xMin > xMax) {
+                break;
+            }
+            while (x <= xMax) {
+                res.add(matrix[x++][y]);
+            }
+            --x;
+            --y;
+            --yMax;
+            if (yMin > yMax) {
+                break;
+            }
+            while (y >= yMin && yMin <= yMax) {
+                res.add(matrix[x][y--]);
+            }
+            ++y;
+            --x;
+            --xMax;
+            if (xMin > xMax) {
+                break;
+            }
+            while (x >= xMin && xMin <= xMax) {
+                res.add(matrix[x--][y]);
+            }
+            ++x;
+            ++y;
+            ++yMin;
+            if (yMin > yMax) {
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().spiralOrder(new int[][]{
+                new int[]{1,2}
+        }));
+    }
+
+}
