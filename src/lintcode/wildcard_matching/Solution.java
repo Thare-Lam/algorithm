51f376ff37c640dbc696f2911f8edf9ae2acd421
Thare <tharelam216@gmail.com>
2017-09-16 17:29:38 +0800

    update lintcode.wildcard_matching

--- src/lintcode/wildcard_matching/Solution.java
+++ src/lintcode/wildcard_matching/Solution.java
@@ -1,37 +1,33 @@
 package lintcode.wildcard_matching;
 
 public class Solution {
+
     /*
      * @param s: A string
      * @param p: A string includes "?" and "*"
      * @return: is Match?
      */
     public boolean isMatch(String s, String p) {
-        int indexS = 0;
-        int indexP = 0;
-        int indexStar = -1;
-        int indexBackupS = 0;
-        int lenS = s.length();
-        int lenP = p.length();
-        while (indexS < lenS) {
-            if (indexP < lenP && (s.charAt(indexS) == p.charAt(indexP) || p.charAt(indexP) == '?')) {
-                ++indexS;
-                ++indexP;
-            } else if (indexP < lenP && p.charAt(indexP) == '*') {
-                indexStar = indexP;
-                ++indexP;
-                indexBackupS = indexS;
-            } else if (indexStar >= 0) {
-                indexP = indexStar + 1;
-                ++indexBackupS;
-                indexS = indexBackupS;
-            } else {
-                return false;
+        return isMatch(s.toCharArray(), p.toCharArray());
+    }
+
+    public boolean isMatch(char[] s, char[] p) {
+        boolean[][] dp = new boolean[s.length + 1][p.length + 1];
+        dp[0][0] = true;
+        int i, j;
+        for (j = 1; j <= p.length && dp[0][j - 1]; ++j) {
+            dp[0][j] = p[j - 1] == '*';
+        }
+        for (i = 1; i <= s.length; ++i) {
+            for (j = 1; j <= p.length; ++j) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j - 1] || dp[i][j - 1] || dp[i - 1][j];
+                } else if (p[j - 1] == '.' || p[j - 1] == s[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
             }
         }
-        while (indexP < lenP && p.charAt(indexP) == '*') {
-            ++indexP;
-        }
-        return indexP == lenP;
+        return dp[s.length][p.length];
     }
+
 }
