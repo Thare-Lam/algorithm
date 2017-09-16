ae5f11e5f0f2a8ba3b74ed179da3922bdfb38fe4
Thare <tharelam216@gmail.com>
2017-09-16 15:12:54 +0800

    ac lintcode.backpack_ii

---
+++ src/lintcode/backpack_ii/Solution.java
@@ -0,0 +1,23 @@
+package lintcode.backpack_ii;
+
+public class Solution {
+    /*
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @param V: Given n items with value V[i]
+     * @return: The maximum value
+     */
+    public int backPackII(int m, int[] A, int[] V) {
+        int n = A.length;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= A[i - 1]) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - A[i - 1]] + V[i - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
