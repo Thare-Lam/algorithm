54ab65a9a5496ba897295c6b83399086f6cdedc5
Thare <tharelam216@gmail.com>
2017-09-19 01:07:37 +0800

    add company.u51._20170918

--- src/company/u51/_20170918/_02/Main.java
+++ src/company/u51/_20170918/_02/Main.java
@@ -1,4 +1,65 @@
 package company.u51._20170918._02;
 
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        Queue<String> queue = new LinkedList<>();
+        MyRunnalbe myRunnalbe = new MyRunnalbe(queue);
+        Thread t1 = new Thread(myRunnalbe);
+        Thread t2 = new Thread(myRunnalbe);
+        Thread t3 = new Thread(myRunnalbe);
+        t1.start();
+        t2.start();
+        t3.start();
+        int n = 200000;
+        while (n-- > 0) {
+            queue.add(in.next());
+        }
+        Thread.sleep(500);
+        System.out.println(myRunnalbe.res);
+    }
+
+    static class MyRunnalbe implements Runnable {
+
+        Queue<String> queue;
+
+        volatile int count;
+
+        int res;
+
+        public MyRunnalbe(Queue<String> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            String str;
+            while (count < 200000) {
+                str = readLine();
+                if (!str.equals("")) {
+                    process(str);
+                }
+            }
+        }
+
+        private synchronized String readLine() {
+            if (!queue.isEmpty()) {
+                ++count;
+                return queue.poll();
+            }
+            return "";
+        }
+
+        private void process(String s) {
+            if (s.contains("u51")) {
+                synchronized(this) {
+                    ++res;
+                }
+            }
+        }
+    }
+
 }
