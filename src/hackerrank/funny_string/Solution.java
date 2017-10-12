5607820316995420241f56a0429af6d7f13bb128
Thare <tharelam216@gmail.com>
2017-10-12 16:59:37 +0800

    ac hackerrank.funny_string

---
+++ src/hackerrank/funny_string/Solution.java
@@ -0,0 +1,26 @@
+package hackerrank.funny_string;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    static String funnyString(String s){
+        for (int len = s.length(), i = 1, j = len - 2; i < len && j >= 0; ++i, --j) {
+            if (Math.abs(s.charAt(i) - s.charAt(i - 1)) != Math.abs(s.charAt(j) - s.charAt(j + 1))) {
+                return "Not Funny";
+            }
+        }
+        return "Funny";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            String s = in.next();
+            String result = funnyString(s);
+            System.out.println(result);
+        }
+    }
+
+}
