6216728c09bae0d90c747e2bc36ab8e7d962a215
Thare <tharelam216@gmail.com>
2017-10-12 22:03:08 +0800

    ac nowcoder.coding_interviews.e02fdb54d7524710a7d664d082bb7811

---
+++ src/nowcoder/coding_interviews/e02fdb54d7524710a7d664d082bb7811/Solution.java
@@ -0,0 +1,28 @@
+package nowcoder.coding_interviews.e02fdb54d7524710a7d664d082bb7811;
+
+public class Solution {
+
+    public void FindNumsAppearOnce(int[] array,int[] num1 , int[] num2) {
+        int xor = 0;
+        for (int a : array) {
+            xor ^= a;
+        }
+        int x = find(xor);
+        for (int a : array) {
+            if ((a & x) == 0) {
+                num1[0] ^= a;
+            } else {
+                num2[0] ^= a;
+            }
+        }
+    }
+
+    private int find(int n) {
+        int a = 1;
+        while ((n & a) == 0) {
+            a <<= 1;
+        }
+        return a;
+    }
+
+}
