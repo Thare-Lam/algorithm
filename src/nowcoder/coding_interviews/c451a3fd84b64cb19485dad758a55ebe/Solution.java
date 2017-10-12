c720603ced07fd9b63a721063420dd34fc3e8c16
Thare <tharelam216@gmail.com>
2017-10-12 22:31:20 +0800

    ac nowcoder.coding_interviews.c451a3fd84b64cb19485dad758a55ebe

---
+++ src/nowcoder/coding_interviews/c451a3fd84b64cb19485dad758a55ebe/Solution.java
@@ -0,0 +1,37 @@
+package nowcoder.coding_interviews.c451a3fd84b64cb19485dad758a55ebe;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Solution().FindContinuousSequence(3);
+    }
+
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        int len = (sum + 1) / 2;
+        int s = 0;
+        int i = 1;
+        int j = 1;
+        while (j <= len) {
+            while (s < sum && j <= len) {
+                s += j++;
+            }
+            while (s > sum) {
+                s -= i++;
+            }
+            if (s == sum && j - i > 1) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int a = i; a < j; ++a) {
+                    list.add(a);
+                }
+                res.add(list);
+                s -= i++;
+            }
+        }
+        System.out.println(res);
+        return res;
+    }
+
+}
